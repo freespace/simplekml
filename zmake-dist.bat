05df6b2e5a209dfe1b5b6d558cde6391aee0fe87
Kyle Lancaster <kyle.lan@gmail.com>
2016-03-18 17:38:38 +0200

    Cleaned up vs commit.

--- zmake-dist.bat
+++
@@ -1,9 +0,0 @@
-py setup.py sdist 
-py setup.py sdist bdist_wininst --target-version="2.6"
-py setup.py sdist bdist_wininst --target-version="2.7"
-py setup.py sdist bdist_wininst --target-version="3.0"
-py setup.py sdist bdist_wininst --target-version="3.1"
-py setup.py sdist bdist_wininst --target-version="3.2"
-py setup.py sdist bdist_wininst --target-version="3.3"
-py setup.py sdist bdist_wininst --target-version="3.4"
-py setup.py sdist bdist_wininst --target-version="3.5"
